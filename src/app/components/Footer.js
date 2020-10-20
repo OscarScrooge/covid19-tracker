068928360b2db5d770fc64f825007e601711072d
OscarScrooge <link57795@gmail.com>
2020-10-19 19:28:21 -0500

    covid19-tracker

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -5,7 +5,7 @@ function Footer(props) {
     return (
             <footer className="footer">
                 <h6>Data provided by <a href="https://covid19-api.org/" target="_blank">covid19-api.org</a></h6>
-                <h6><a href="https://github.com/OscarScrooge?tab=repositories" target="_blank">@OscarScrooge</a></h6>
+                <h6><a href="https://github.com/OscarScrooge/covid19-tracker" target="_blank">@OscarScrooge</a></h6>
             </footer>
     );
 }
